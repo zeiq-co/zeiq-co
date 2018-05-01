554be29ae6f6720686ba4df83720bc3f14fcdf63
vipin kumar <vipinkumar@Parminders-Mac-mini.local>
2018-05-01 17:40:20 +0530

    minor fixes

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -43,6 +43,34 @@ const IndexLayout = ({ children, data }) => {
       />
 
       <Header />
+
+      {/* meta={[
+            // Search Engine
+            { name: 'image', content: image },
+            { name: 'description', content: description },
+            { itemprop: 'description', content: description },
+            { itemprop: 'image', content: image },
+            { name: 'author', content: APP_AUTHOR },
+
+            // Twitter
+            { name: 'twitter:card', content: 'summary_large_image' },
+            { name: 'twitter:site', content: twitter },
+            { name: 'twitter:description', content: description },
+            { name: 'twitter:title', content: APP_NAME },
+            { name: 'twitter:creator', content: APP_AUTHOR },
+            { name: 'twitter:image:src', content: image },
+
+            // Open Graph general (Facebook, Linkdin & Google+)
+            { name: 'og:title', content: APP_NAME },
+            { name: 'og:image', content: image },
+            { name: 'og:site_name', content: APP_NAME },
+            { name: 'og:description', content: description },
+            { name: 'og:locale', content: 'en_us' },
+            { name: 'fb:admins', content: fbUserId },
+            { name: 'fb:app_id', content: fbAppId },
+            { name: 'og:type', content: type },
+            { name: 'og:url', content: SITE_URL },
+          ]} */}
       {children()}
       <Footer social={social} />
     </div>
