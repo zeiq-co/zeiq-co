f5b185332213822aeecd24c9034c726dadbc1c5f
vipin kumar <vipinkumar@Parminders-Mac-mini.local>
2018-05-01 15:53:24 +0530

    added metatags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -18,22 +18,30 @@ const IndexLayout = ({ children, data }) => {
           // Search Engine
           { name: 'image', content: config.image },
           { name: 'description', content: config.description },
+          { itemprop: 'description', content: config.description },
+          { itemprop: 'image', content: config.image },
 
           // Twitter
+          { name: 'twitter:card', content: 'summary_large_image' },
           { name: 'twitter:site', content: config.twitterSite },
+          { name: 'twitter:description', content: config.description },
+          { name: 'twitter:title', content: config.siteName },
           { name: 'twitter:creator', content: config.twitterCreator },
           { name: 'twitter:image:src', content: config.image },
 
           // Open Graph general (Facebook, Linkdin & Google+)
-
+          { name: 'og:title', content: config.siteName },
           { name: 'og:image', content: config.image },
           { name: 'og:site_name', content: config.siteName },
+          { name: 'og:description', content: config.description },
           { name: 'og:locale', content: config.locale },
           { name: 'fb:admins', content: config.fbUserId },
           { name: 'fb:app_id', content: config.fbAppId },
           { name: 'og:type', content: config.type },
+          { name: 'og:url', content: config.siteUrl },
         ]}
       />
+
       <Header />
       {children()}
       <Footer social={social} />
