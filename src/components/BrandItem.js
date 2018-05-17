02637dd5591956b2a4375161dc62e3b32fe72fbd
vipin kumar <vipinkumar@Parminders-Mac-mini.local>
2018-05-17 17:55:52 +0530

    minor fixes

--- src/components/BrandItem.js
+++ src/components/BrandItem.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link';
 
 const BrandItem = ({ data }) => (
   <div className="col-6 col-md-4 col-lg-3">
-    <a href="#" className="brand-item eq-height">
+    <a className="brand-item eq-height" style={{ height: 197.875 }}>
       <img src={data.image.file.url} alt={data.name} />
     </a>
   </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -84,7 +84,7 @@ export const query = graphql`
         }
       }
     }
-    allContentfulBrand {
+    allContentfulBrand(sort: { fields: [order] }) {
       edges {
         node {
           id
