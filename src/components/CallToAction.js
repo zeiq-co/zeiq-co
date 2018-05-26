7aa5d6e8949b325b6aa3c7424cf4237f0b8b6dc1
vipin kumar <vipinkumar@Parminders-Mac-mini.local>
2018-05-26 17:05:32 +0530

    issue fixe

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link';
 
 const CallToAction = () => (
   <div className="call-to-action text-left">
-    <h4 className="text-color">Challange the Stable!</h4>
+    <h4 className="text-color">Challenge the Stable!</h4>
     <div className="vertical-middle">
       <h1>Let us see how we can help you.</h1>
       <Link to="/contact/">
