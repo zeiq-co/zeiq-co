270edfb2e5c08ab9315caf2a12407e1204372106
Anushka Beri <berianushka@gmail.com>
2019-05-03 14:50:13 +0530

    Creates Jobs  and openSource Page, no content added till now

---
+++ src/pages/jobs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Jobs extends React.Component{
+    render(){
+        return(
+            <h3>Jobs Page content goes here.</h3>
+        )
+    }
+}
