14add4cd0c5d36787b27c4af89305af2654671f9
Anushka Beri <berianushka@gmail.com>
2019-05-16 13:00:35 +0530

    minor fixes

--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import WhatYouNeedIsNotListed from '../components/WhatYouNeedIsNotListed';
+import Seo from '../components/Seo';
+import ServicesTitle from '../components/ServicesTitle';
+import config from '../config';
+import ServicesNavigation from '../components/ServicesNavigation';
+
 export default class Jobs extends React.Component{
     render(){
         return(
-            <h3>Jobs Page content goes here.</h3>
+    <div>
+        <Seo
+            title="Services"
+            description="Zeiq offers a wide array of services related to developing a website.If something you expect us to do is not listed here,do get in touch!"
+            url={config.siteUrl}
+            image="/img/favicon.png"
+        />
+        <main id="content" className="white-background">
+            <div className="container">
+            <div className="row">
+                <div className="col-12">
+                <ServicesTitle />
+                <WhatYouNeedIsNotListed />
+                </div>
+            </div>
+            </div>
+        </main>
+    </div>
         )
     }
 }
