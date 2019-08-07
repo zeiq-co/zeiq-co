f67f6e7dea938fb675a1f4aec9454c060e38564b
gagansaini1212 <gagansaini1212@gmail.com>
2019-08-07 14:23:47 +0530

    removed background

--- src/components/AppScreens.js
+++ src/components/AppScreens.js
@@ -21,7 +21,7 @@ const Container = styled.div`
   }
   img {
     margin-top: 2rem;
-    border-style: outset;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   }
 `;
 
--- src/pages/reactNativeApp.js
+++ src/pages/reactNativeApp.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import Seo from '../components/Seo';
 import config from '../config';
 import ReactNativeAppContent from '../components/ReactNativeAppContent';
 import AppScreens from '../components/AppScreens';
-
-const Wrapper = styled.div`
-  background: #f6f5fd;
-`;
 
 export default class ReactNativeApp extends React.Component {
   render() {
@@ -22,12 +17,12 @@ export default class ReactNativeApp extends React.Component {
           image="/img/favicon.png"
         />
         <main id="content" className="white-background">
-          <Wrapper className="container">
+          <div className="container">
             <div className="row">
               <ReactNativeAppContent />
               <AppScreens />
             </div>
-          </Wrapper>
+          </div>
         </main>
       </div>
     );
