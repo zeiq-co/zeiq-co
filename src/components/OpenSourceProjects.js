e78131e11f0f95d7922f9233eb8a7f3557731e54
Parminder Klair <perminder.klair@gmail.com>
2019-12-19 12:13:40 +0530

    open source styling

--- src/components/OpenSourceProjects.js
+++ src/components/OpenSourceProjects.js
@@ -36,27 +36,31 @@ const OpenSourceProjects = () => {
   // console.log('projects', result);
 
   return (
-    <Container className="col-12">
-      <div className="grid clearfix">
-        {result.map(({ node }) => (
-          <Item className="grid-item post has-thumbnail" key={node.id}>
-            <div className="post-details">
-              <h4>
-                <a href={node.frontmatter.url}>{node.frontmatter.title}</a>
-              </h4>
-              <p>{node.frontmatter.info}</p>
-              <p className="meta clearfix">
-                <a href={node.frontmatter.url} className="read-more">
-                  Details <i className="saulticon-arrow-forward" />
-                </a>
-              </p>
-            </div>
-          </Item>
-        ))}
-        <div className="grid-sizer" />
-        <div className="gutter-sizer" />
+    <div className="container">
+      <div className="row blog-listing no-sidebar">
+        <Container className="col-12">
+          <div className="grid clearfix">
+            {result.map(({ node }) => (
+              <Item className="grid-item post has-thumbnail" key={node.id}>
+                <div className="post-details">
+                  <h4>
+                    <a href={node.frontmatter.url}>{node.frontmatter.title}</a>
+                  </h4>
+                  <p>{node.frontmatter.info}</p>
+                  <p className="meta clearfix">
+                    <a href={node.frontmatter.url} className="read-more">
+                      Details <i className="saulticon-arrow-forward" />
+                    </a>
+                  </p>
+                </div>
+              </Item>
+            ))}
+            <div className="grid-sizer" />
+            <div className="gutter-sizer" />
+          </div>
+        </Container>
       </div>
-    </Container>
+    </div>
   );
 };
 
