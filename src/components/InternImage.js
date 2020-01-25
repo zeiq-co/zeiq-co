7b96a359c886fcb2d5a809ae3e226f5de255c105
iamkunall <103kunalsharma@gmail.com>
2020-01-25 12:28:52 +0530

    intern page image added

---
+++ src/components/InternImage.js
@@ -0,0 +1,11 @@
+/* eslint jsx-a11y/heading-has-content: 0 */
+
+import React from 'react';
+
+const InternImage = () => (
+  <figure className="image is-128x128">
+    <img src="/images/content-writing.png" />
+  </figure>
+);
+
+export default InternImage;
