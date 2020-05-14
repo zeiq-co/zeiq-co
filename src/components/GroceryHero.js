a4658201e3245f173913ed8e8c6a180a7935bb09
dharmveer97 <dvbangar8@gmail.com>
2020-05-14 17:15:40 +0530

    minor fixes.

--- src/components/GroceryHero.js
+++ src/components/GroceryHero.js
@@ -17,11 +17,12 @@ const GroceryHero = () => (
       <div className="row align-items-center">
         <div className="col-lg-6 aos-init aos-animate">
           <div className="jumbotron">
-            <h1 className="page-header-title">
-              Build your next project faster with SB UI Kit Pro
-            </h1>
+            <h1 className="page-header-title">ZEIQ DELIVERY APP</h1>
             <p className="lead page-header-text mb-5">
-             Are you looking to launch your online Food Shop, Grocery Shop, or an eCommerce mobile app for your supermarket business? Then this readymade flutter app will help you to build your business with your own mobile app in just
+              Are you looking to launch your online Food Shop, Grocery Shop, or
+              an eCommerce mobile app for your supermarket business? Then this
+              readymade flutter app will help you to build your business with
+              your own mobile app in just
             </p>
             <p className="lead">
               <a className="button has-arrow" href="/services/">
