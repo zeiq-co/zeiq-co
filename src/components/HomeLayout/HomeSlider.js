00bf42986728dfcc2433cf9af4d2d02af9e3f059
karanbir8872 <wwwkaranbir786@gmail.com>
2020-09-08 15:58:21 +0530

    minor fixes

--- src/components/HomeLayout/HomeSlider.js
+++ src/components/HomeLayout/HomeSlider.js
@@ -5,16 +5,17 @@ const HomeSlider = ({ data }) => (
   <div className="slider-activation">
     <div
       className="slide slide-style-1 slider-fixed--height d-flex align-items-center bg_image bg_image--1"
-      data-black-overlay="6">
+      data-black-overlay="6"
+    >
       <div className="container position-relative">
         <div className="row">
-          <div className="col-lg-12">
+          <div className="col-lg-10">
             <div className="inner">
-              <h1 className="title theme-gradient">{data.slogan}</h1>
+              <h1 className="title theme-gradient mt--80">{data.slogan}</h1>
             </div>
           </div>
         </div>
-        <div className="service-wrapper service-white">
+        <div className="service-wrapper service-white mt--40">
           <ServiceOne data={data.features} />
         </div>
       </div>
