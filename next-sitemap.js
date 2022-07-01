24d8d3f2161e29d56d04f067c4537accd1d73e22
dharmveer97 <dvbangar8@gmail.com>
2022-07-01 13:46:24 +0530

    sitemap added

---
+++ next-sitemap.js
@@ -0,0 +1,33 @@
+const siteUrl = `https://zeiq-co-git-v4-zeiq-dev.vercel.app`;
+
+module.exports = {
+  siteUrl,
+  changefreq: 'daily',
+  sitemapSize: 1000,
+  generateRobotsTxt: true,
+  exclude: [
+    '/index',
+    '/about',
+    '/blog',
+    '/services',
+    '/work',
+    '/about',
+    '/contact',
+    '/products',
+    '/support',
+    '/blog/sitemap.xml',
+    '/work/sitemap.xml',
+    '/technology/sitemap.xml',
+    '/page/sitemap.xml',
+    '/services/sitemap.xml',
+  ],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `${siteUrl}/page/sitemap.xml`,
+      `${siteUrl}/work/sitemap.xml`,
+      `${siteUrl}/technology/sitemap.xml`,
+      `${siteUrl}/blog/sitemap.xml`,
+      `${siteUrl}/services/sitemap.xml`,
+    ],
+  },
+};
