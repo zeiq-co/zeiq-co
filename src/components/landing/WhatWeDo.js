8f6db240e1995e38be9fb1d79da318ae9f369fdc
dharmveer97 <dvbangar8@gmail.com>
2022-09-08 14:25:59 +0530

    minor fix

--- src/components/landing/WhatWeDo.js
+++ src/components/landing/WhatWeDo.js
@@ -32,63 +32,64 @@ const WhatWeDo = () => (
     <section className="section pricing-plans bg-white">
       <div className="container">
         <div className="row no-gutters justify-content-center align-items-center">
-          {data.map((item) => (
-            <div
-              className="col-xl-4 col-lg-5 col-md-6 mt-4 pt-2 mt-md-0 pt-md-0 has-anim fade anim-delay-1"
-              key={item.id}
-            >
+          {data &&
+            data.map((item) => (
               <div
-                className={`block border mx-2 my-2 ${
-                  item.isDark ? 'bg-dark' : ''
-                }`}
+                className="col-xl-3 col-lg-4 col-md-6 mt-4 pt-2 mt-md-0 pt-md-0 has-anim fade anim-delay-1"
+                key={item.id}
               >
-                <h5
-                  className={`font-weight-500 ${item.isDark ? 'bg-dark' : ''}`}
-                >
-                  Standard subscription
-                </h5>
-                <div className="price-wrapper">
-                  <span
-                    className={`price ${
-                      item.isDark ? 'text-white' : 'text-dark'
-                    }`}
-                  >
-                    $3,500
-                  </span>
-                  <span>/ month</span>
-                </div>
                 <div
-                  className={`content  ${
-                    item.isDark
-                      ? 'text-light content-dark'
-                      : 'text-dark content-light'
-                  }`}
+                  className={`block border  ${item.isDark ? 'bg-dark' : ''}`}
                 >
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Velit amet suscipit non.
-                  </p>
-                  <h6>What you get with Standard</h6>
-                  <ul className="pl-0">
-                    <li>10 User</li>
-                    <li>Unlimited Bandwidth</li>
-                    <li>Whitelabelling</li>
-                    <li>Priority Support</li>
-                    <li>File Uploader</li>
-                  </ul>
-                  <a
-                    href="contact.html"
-                    className={`btn btn-block mt-3 ${
-                      item.isDark ? 'btn-white' : 'btn-black'
+                  <h5
+                    className={`font-weight-500 ${
+                      item.isDark ? 'bg-dark' : ''
                     }`}
-                    data-text="I Want This"
                   >
-                    <span>I Want This</span>
-                  </a>
+                    Standard subscription
+                  </h5>
+                  <div className="price-wrapper">
+                    <span
+                      className={`price ${
+                        item.isDark ? 'text-white' : 'text-dark'
+                      }`}
+                    >
+                      $3,500
+                    </span>
+                    <span>/ month</span>
+                  </div>
+                  <div
+                    className={`content  ${
+                      item.isDark
+                        ? 'text-light content-dark'
+                        : 'text-dark content-light'
+                    }`}
+                  >
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Velit amet suscipit non.
+                    </p>
+                    <h6>What you get with Standard</h6>
+                    <ul className="pl-0">
+                      <li>10 User</li>
+                      <li>Unlimited Bandwidth</li>
+                      <li>Whitelabelling</li>
+                      <li>Priority Support</li>
+                      <li>File Uploader</li>
+                    </ul>
+                    <a
+                      href="contact.html"
+                      className={`btn btn-block mt-3 ${
+                        item.isDark ? 'btn-white' : 'btn-black'
+                      }`}
+                      data-text="I Want This"
+                    >
+                      <span>I Want This</span>
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </section>
