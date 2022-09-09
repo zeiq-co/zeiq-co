f3406c766f7635cb05dbb5748f0a64bfcabfd028
dharmveer97 <dvbangar8@gmail.com>
2022-09-09 11:37:49 +0530

    minor fix

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -7,7 +7,7 @@ const Faq = () => (
     <div className="row justify-content-center">
       <div>
         <div className="mb-4">
-          <h4 className="text-dark mb-3"> Frequently Asked Questions</h4>
+          <h4 className="text-dark mb-3">Frequently Asked Questions</h4>
           <p>
             Share the details of your project – like scope, time frames, or
             business challenges. Our team will carefully study them and then
