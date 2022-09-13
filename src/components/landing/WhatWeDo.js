d3ac1b0842b8322b8a5dd88ceb3bb698225e65df
dharmveer97 <dvbangar8@gmail.com>
2022-09-13 21:57:35 +0530

    fixed cards

--- src/components/landing/WhatWeDo.js
+++ src/components/landing/WhatWeDo.js
@@ -1,30 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-// import Link from 'next/link';
-
-const Wrapper = styled.div`
-  .pricing-plans .block {
-    padding: 40px;
-  }
-  @media (max-width: 767px) {
-    .pricing-plans .block {
-      padding: 30px 30px;
-    }
-  }
-  @media (max-width: 575px) {
-    .pricing-plans .block {
-      padding: 40px 20px;
-    }
-  }
-  .pricing-plans .price-wrapper {
-    margin: 25px 0;
-  }
-  .pricing-plans .price-wrapper .price {
-    font-size: 48px;
-    line-height: 56px;
-    font-weight: 700;
-  }
-`;
 
 const data = [
   {
@@ -70,59 +44,55 @@ const data = [
 ];
 
 const WhatWeDo = () => (
-  <Wrapper>
-    <section className="section pricing-plans bg-white">
-      <div className="container">
-        <div className="col-12 has-anim fade">
-          <div className="section-title text-center">
-            <h2 className="title text-dark">What We Do ?</h2>
-            <p className="font-weight-700">
-              We understand how to deliver engaging and successful eCommerce
-              experiences
-            </p>
-          </div>
-        </div>
-        <div className="row no-gutters justify-content-center align-items-center">
-          {data &&
-            data.map((item, i) => (
-              <div
-                className="col-xl-3 col-lg-4 col-md-6 mt-4 pt-2 mt-md-0 pt-md-0 has-anim fade anim-delay-1"
-                key={item.id || i}
-              >
-                <div
-                  className={`block border  ${item.isDark ? 'bg-dark' : ''}`}
-                >
-                  <div className="price-wrapper">
-                    <h3
-                      className={`font-weight-100 font ${
-                        item.isDark ? 'text-white' : 'text-dark'
-                      }`}
-                    >
-                      {item.title}
-                    </h3>
-                  </div>
-                  <div
-                    className={`content  ${
-                      item.isDark
-                        ? 'text-light content-dark'
-                        : 'text-dark content-light'
-                    }`}
-                  >
-                    <ul className="pl-0">
-                      {item &&
-                        item.list &&
-                        item.list.map((el) => (
-                          <li key={el.title}>{el.title}</li>
-                        ))}
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            ))}
+  // <Wrapper>
+  <section className="section pricing-plans bg-white">
+    <div className="container">
+      <div className="col-12 has-anim fade">
+        <div className="section-title text-center">
+          <h2 className="title text-dark">What We Do ?</h2>
+          <p className="font-weight-700">
+            We understand how to deliver engaging and successful eCommerce
+            experiences
+          </p>
         </div>
       </div>
-    </section>
-  </Wrapper>
+      <div className="row no-gutters justify-content-center align-items-center">
+        {data &&
+          data.map((item, i) => (
+            <div
+              className="col-lg-4 col-md-6 mt-4 pt-2 mt-md-0 pt-md-0 has-anim fade anim-delay-1"
+              key={item.id || i}
+            >
+              <div className={`block border  ${item.isDark ? 'bg-dark' : ''}`}>
+                <div className="price-wrapper">
+                  <h3
+                    className={`font-weight-100 font ${
+                      item.isDark ? 'text-white' : 'text-dark'
+                    }`}
+                  >
+                    {item.title}
+                  </h3>
+                </div>
+                <div
+                  className={`content  ${
+                    item.isDark
+                      ? 'text-light content-dark'
+                      : 'text-dark content-light'
+                  }`}
+                >
+                  <ul className="pl-0">
+                    {item &&
+                      item.list &&
+                      item.list.map((el) => <li key={el.title}>{el.title}</li>)}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  </section>
+  // </Wrapper>
 );
 
 export default WhatWeDo;
