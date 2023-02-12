16339592d789f007e288498ef66c9f3cfa56a8c5
dharmveer97 <dvbangar8@gmail.com>
2023-02-12 13:15:06 +0530

    minor fix

--- src/components/home/HomeServices.js
+++ src/components/home/HomeServices.js
@@ -45,13 +45,13 @@ const HomeServices = ({ data, category, isServices, title, subtitle }) => {
                       ━━
                     </span>
                   )}
-                  {item.linkTo && (
+                  {/* {item.linkTo && (
                     <div>
                       <Link href={`${item.linkTo || '/'}`}>
                         <a>Learn more</a>
                       </Link>
                     </div>
-                  )}
+                  )} */}
                 </div>
               </div>
             ))}
